6c6d06a83332d79d25c32adb6e79223191899728
JuergenSchneider25 <juergenschneider2512@gmail.com>
2022-04-27 09:09:45 +0200

    update1

--- index.php
+++ index.php
@@ -1,34 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="js/main.js"></script>
-</head>
-
-<body>
-    <header></header>
-    <main>
-        <div>Welcome To Quiz Plus</div>
-        <form action="/q1.php" method="post">
-
-            <button type="submit">Start</button>
-        </form>
-    </main>
+//Inilieze session.
+session_start();
 
 
-    <footer></footer>
-
-    <?php
-    echo "QuizzPlus!";
-    ?>
-    <?php
+function getQuestions()
+{
 
     // connect to mySQL database using PHP PDO Object
     $dbName = getenv('DB_NAME');
@@ -52,6 +29,7 @@
     // print_r($questions);
 
     foreach ($questions as $key => $question) {
+
         // prepare an SQL statement with a placeholder ? the help of the db connection $dbConnection
         $subQuery = $dbConnection->prepare("SELECT * from Answer where Answer.QuestionId = ? ");
         $subQuery->bindValue(1, $question['ID']);
@@ -60,44 +38,106 @@
         $questions[$key]['answers'] = $answers;
     }
 
+    return $questions;
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/main.js"></script>
+</head>
+
+<body>
+
+    <main>
+        <div>Welcome To Quiz Plus</div>
+        <form action="/q1.php" method="post">
+
+            <button type="submit">Start</button>
+        </form>
+    </main>
+
+
+
+
+    <?php
+    echo "QuizzPlus!";
+    ?>
+    <?php
+
+
+    //Evaluate data in $_Post variable
+    $currentQuestionIndex = 0;
+
+    if (isset($_POST['lastQuestionIndex'])) {
+        // get data from last post.
+        $lastQuestionIndex = $_POST['lastQuestionIndex'];
+
+        if (isset($_POST['nextQuestionIndex'])) {
+            //Define the index number of the next question.
+            $currentQuestionIndex = $_Post['nextQuestionIndex'];
+        }
+    }
+
+    // check if $_SESSION questions exists
+    if (!isset($_SESSION['questions'])) {
+        // echo questions data EXISTS in session <br>`;
+
+        //..... and save data in $_SESSION
+        $_SESSION['questions'] = getQuestions();
+    }
+
+    echo '<pre>';
+    print_r($_SESSION['questions']);
+    echo '</pre>';
+
     //DevOnly: Debug output to see what is inside the array $question
-    print "<pre/>";
-    print_r($questions);
+   
     exit();
 
 
     ?>
 
-<div class ="row">
+    <div class="row">
         <div class="col-sm-12">
-        <h3> Frage <?php echo $currentQuestionIndex;?></h3>
-        <p><?php echo $questions[$currentQuestionIndex]['text']; ?></p>
+            <h3> Frage <?php echo $currentQuestionIndex; ?></h3>
+            <p><?php echo $questions[$currentQuestionIndex]['text']; ?></p>
             <form method="post>">
                 <div class="form-check">
-                    <input class="form-check-input" type = "checkbox" value ="0" id="flexCheckDefault">
-                    <label class="form-check-label" for = "flexCheckDefault">
-                    <?php
+                    <input class="form-check-input" type="checkbox" value="0" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                        <?php
                         $Answers = $questions[$currentQuestionIndex]['Answers'];
                         $Answers = $Answers[0];
                         echo $Answers['Answer'];
                         ?>
-                        </label>
-                            </div>
-                            <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="1" id="flexCheckChecked";
-                            label class="form-check-label" for="flexCheckChecked">
-                            <?php
-                            $answers = $questions[$currentQuestionIndex]['answers'];
-                            echo $answers[1]['answer'];
-                        ?>
-                        </label>
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="1" id="flexCheckChecked" ; label class="form-check-label" for="flexCheckChecked">
+                    <?php
+                    $answers = $questions[$currentQuestionIndex]['answers'];
+                    echo $answers[1]['answer'];
+                    ?>
+                    </label>
 
 
-                <input types="hidden" names="lastQuestionIndex" value=" <?php echo $currentQuestionIndex;?>">
-                <input types="hidden" names="nextQuestionIndex" value=" <?php echo $currentQuestionIndex + 1;?>">
-                    
+                    <input types="hidden" names="lastQuestionIndex" value=" <?php echo $currentQuestionIndex; ?>">
+                    <input types="hidden" names="nextQuestionIndex" value=" <?php echo $currentQuestionIndex + 1; ?>">
+
 
 
 
 </body>j
+
 </html>
